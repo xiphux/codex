ed44970a86a41e81f6848e49eeaa390d807b0a42
Chris Han <xiphux@gmail.com>
2008-08-05 00:37:40 -0400

    Limit is not portable

--- include/matchup_data.php
+++ include/matchup_data.php
@@ -12,7 +12,7 @@
 function matchup_data($id)
 {
 	global $tables;
-	return DBGetRow("SELECT m1.matchup_id, char1.character_name AS match1, char1.character_id AS id1, char2.character_name AS match2, char2.character_id AS id2, CONCAT(char1.character_name,' + ',char2.character_name) AS matchup_name FROM " . $tables['matchups'] . " AS m1 LEFT JOIN " . $tables['characters'] . " AS char1 ON char1.character_id = m1.match_1 LEFT JOIN " . $tables['characters'] . " AS char2 ON char2.character_id = m1.match_2 WHERE m1.matchup_id = " . $id . " LIMIT 1");
+	return DBGetRow("SELECT m1.matchup_id, char1.character_name AS match1, char1.character_id AS id1, char2.character_name AS match2, char2.character_id AS id2, CONCAT(char1.character_name,' + ',char2.character_name) AS matchup_name FROM " . $tables['matchups'] . " AS m1 LEFT JOIN " . $tables['characters'] . " AS char1 ON char1.character_id = m1.match_1 LEFT JOIN " . $tables['characters'] . " AS char2 ON char2.character_id = m1.match_2 WHERE m1.matchup_id = " . $id);
 }
 
 ?>
