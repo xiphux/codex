c87fd970cba0f29dfab8a4e1b0e26b128b7e4776
Chris Han <xiphux@gmail.com>
2009-10-11 16:43:31 -0500

    Avoid double spaces when unwrapping lines

--- include/readchapter.php
+++ include/readchapter.php
@@ -63,7 +63,7 @@ function readchapter($id, $ch = 1)
 			 * Unwrap if specified
 			 */
 			if ($codex_conf['unwrap'] && isset($chapdata['wrapped']) && ($chapdata['wrapped'] === "1"))
-				$fdat = preg_replace("/([^\n])\r\n([^\r\s])/","$1 $2",$fdat);
+				$fdat = preg_replace("/([^\n]) *\r\n([^\r\s])/","$1 $2",$fdat);
 
 			/*
 			 * Pad lines if specified
