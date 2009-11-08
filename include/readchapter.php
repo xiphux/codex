38573e4d301b6206bff536f297253fef132cf4f7
Chris Han <xiphux@gmail.com>
2009-11-08 02:21:55 -0600

    Style superscripted trademark

--- include/readchapter.php
+++ include/readchapter.php
@@ -84,6 +84,7 @@ function readchapter($id, $ch = 1)
 			if ($codex_conf['stylize'] == TRUE) {
 				$fdat = preg_replace("/(\W)_([^\t\n\r\f\a\e>]+?)_(\W)/e", "'$1<span class=\"emphasis\">'.str_replace('_',' ','$2').'</span>$3'", $fdat);
 				$fdat = preg_replace("/([^\*])\*([^\*>\n]*)\*([^\*])/","$1*<span class=\"emphasis\">$2</span>*$3", $fdat);
+				$fdat = preg_replace("/\^?\(TM\)/i", "<sup>TM</sup>", $fdat);
 			}
 
 			$tpl->assign("fdata", $fdat);
